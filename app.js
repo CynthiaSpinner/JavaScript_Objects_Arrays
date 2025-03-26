795f51179fddefb95884aa484ccd604b49af0c0c
Cynthia <CynthiaSpinner@gmail.com>
2025-03-26 16:30:21 -0500

    function that returns book info

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
-//This is a function that takes an array of numbers and returns the sum of those numbers using the forEach method.
+//This is a function that takes an array of numbers and returns the sum of those numbers using the reduce method.
 function arraySum(numbers) {
     let sum = 0;
 
@@ -20,4 +20,15 @@ console.log(arraySum(numbers));
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
+//This is the book object that has a method that returns the book's information.
+const book = {};
+book.title = "Dante's Paradiso";
+book.author = "Dante Alighieri";
+book.pages = 432;
+book.readCount = 1;
 
+book.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, read ${this.readCount} time${this.readCount > 1 ? 's' : ''}.`;
+};
+
+console.log(book.info());
