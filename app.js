c08cc9d7b1aa991e85d66829ca10676dd4e75020
Cynthia <CynthiaSpinner@gmail.com>
2025-03-26 16:25:39 -0500

    function that finds the sum of an array

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+//This is a function that takes an array of numbers and returns the sum of those numbers using the forEach method.
+function arraySum(numbers) {
+    let sum = 0;
+
+    numbers.forEach((number) => {
+        sum += number;
+    });
+
+    return sum;
+}
+
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+console.log(arraySum(numbers));
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
